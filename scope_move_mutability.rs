06aeb4728c9623de52bc7c19dcc6ebe02ebd81c4
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2023-01-28 00:12:18 +0700

    scope move mutability

---
+++ scope_move_mutability.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let immutable_box = Box::new(5u32);
+
+    println!("immutable_box contains {}", immutable_box);
+
+    // mutability error
+    // *immutable_box = 4;
+
+    // *move* the box, changing the ownership (and mutability)
+    let mut mutable_box = immutable_box;
+
+    println!("mutable_box contains {}", mutable_box);
+
+    // modify the contents of the box
+    *mutable_box = 4;
+
+    println!("mutable_box now contains {}", mutable_box);
+}
